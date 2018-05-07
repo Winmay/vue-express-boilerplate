645d51229c1b0f182aa95fd6b96369e9a274d3da
Winmay <mei-tang@qq.com>
2018-05-08 00:03:18 +0800

    '优化配置文件'

--- config/index.js
+++ config/index.js
@@ -30,11 +30,11 @@ module.exports = {
     proxyTable: {
         '/api':{
             // target:'http://jsonplaceholder.typicode.com',
-            target: 'http://localhost:9999',//设置调用接口域名和端口号别忘了加http
+            target: 'http://localhost:3000/api/',//设置调用接口域名和端口号别忘了加http
             changeOrigin:true,
             secure: false,
             pathRewrite:{
-                '/api':'/'
+                '^/api':'/'
             }
             //这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
             // 比如我要调用'http://0.0:300/user/add'，直接写‘/api/user/add’即可 代理后地址栏显示/
